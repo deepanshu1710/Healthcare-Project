00ea28cf17df00809c39eacde607b10fffd83df5
deepanshu1710 <160321407+deepanshu1710@users.noreply.github.com>
2024-08-16 22:56:03 +0530

    Update settings.py

--- healthcare/settings.py
+++ healthcare/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'your-secret-key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['your-render-app.onrender.com', '127.0.0.1', 'localhost','healthcare-project.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 LOGGING = {
