ab2996fc895b64d538afdde35260588eb4ee885b
deepanshu1710 <160321407+deepanshu1710@users.noreply.github.com>
2024-08-16 23:26:14 +0530

    Update settings.py

--- healthcare/settings.py
+++ healthcare/settings.py
@@ -80,8 +80,11 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'HOST': '',  # Leave HOST empty to use the socket
+        'PORT': '',  # Leave PORT empty to use the socket
+        'OPTIONS': {
+            'unix_socket': '/run/mysqld/mysqld.sock',
+        },
     }
 }
 
