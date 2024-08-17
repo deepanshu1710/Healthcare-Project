1d328cd5b14826cf5583655523665a5d5b6846e2
deepanshu1710 <160321407+deepanshu1710@users.noreply.github.com>
2024-08-17 12:30:47 +0530

    Update settings.py

--- healthcare/settings.py
+++ healthcare/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 import logging
+import dj_database_url
 
 print("DB_HOST:", os.getenv('DB_HOST'))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,14 +77,7 @@ WSGI_APPLICATION = 'healthcare.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': 'DB_HOST',  # Set your cloud DB host
-        'PORT': '3306',  # Default MySQL port
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 # Password validation
